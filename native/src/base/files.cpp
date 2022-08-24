6856039835db6a902e898875dee3646159dda8aa
Alex Wilson <alex.wilson1@outlook.com>
2022-08-24 13:10:12 -0800

    Update files.cpp

--- native/src/base/files.cpp
+++ native/src/base/files.cpp
@@ -6,9 +6,70 @@
 
 #include <base.hpp>
 #include <selinux.hpp>
-#include <fstream>
+
+#include <errno.h>
 
 using namespace std;
+
+
+int cp(const char *to, const char *from)
+{
+    int fd_to, fd_from;
+    char buf[4096];
+    ssize_t nread;
+    int saved_errno;
+
+    fd_from = open(from, O_RDONLY);
+    if (fd_from < 0)
+        return -1;
+
+    fd_to = open(to, O_WRONLY | O_CREAT | O_EXCL, 0666);
+    if (fd_to < 0)
+        goto out_error;
+
+    while (nread = read(fd_from, buf, sizeof buf), nread > 0)
+    {
+        char *out_ptr = buf;
+        ssize_t nwritten;
+
+        do {
+            nwritten = write(fd_to, out_ptr, nread);
+
+            if (nwritten >= 0)
+            {
+                nread -= nwritten;
+                out_ptr += nwritten;
+            }
+            else if (errno != EINTR)
+            {
+                goto out_error;
+            }
+        } while (nread > 0);
+    }
+
+    if (nread == 0)
+    {
+        if (close(fd_to) < 0)
+        {
+            fd_to = -1;
+            goto out_error;
+        }
+        close(fd_from);
+
+        /* Success! */
+        return 0;
+    }
+
+  out_error:
+    saved_errno = errno;
+
+    close(fd_from);
+    if (fd_to >= 0)
+        close(fd_to);
+
+    errno = saved_errno;
+    return -1;
+}
 
 ssize_t fd_path(int fd, char *path, size_t size) {
     snprintf(path, size, "/proc/self/fd/%d", fd);
@@ -152,9 +213,7 @@ void cp_afc(const char *source, const char *destination) {
     } else{
         unlink(destination);
         if (S_ISREG(a.st.st_mode)) {
-            ifstream  src(source,  ios::binary);
-            ofstream  dst(destination, ios::binary);
-            dst << src.rdbuf();
+            cp(destination,source)
         } else if (S_ISLNK(a.st.st_mode)) {
             char buf[4096];
             xreadlink(source, buf, sizeof(buf));
